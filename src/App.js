b221a4d52eed4e86d29818eecb1a7b105b467683
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-07-14 14:38:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Info from "./components/Info";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+    </div>
+  );
+}
