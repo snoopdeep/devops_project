014cf7ed2e7707df132ba7b8ccd721ce5fda7499
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-07-14 16:38:25 +0530

    add header test

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -3,6 +3,6 @@ import Header from "../components/Header";
 
 test("renders Header component", () => {
   render(<Header />);
-  const linkElement = screen.getByText(/See DevOps Lab/i);
+  const linkElement = screen.getByText(/See DevOps Lab-test/i);
   expect(linkElement).toBeInTheDocument();
 });
